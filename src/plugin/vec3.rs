2e0f901afcc1bd287898e161ec4df98d4fb00618
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-02-19 12:31:51 -0600

    Add vec3 functions

--- src/plugin/vec3.rs
+++ src/plugin/vec3.rs
@@ -1,4 +1,9 @@
-use super::CmsVEC3 as CmsVEC3;
+#[derive(Copy, Clone, PartialEq, Debug, Default)]
+pub struct CmsVEC3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
 
 impl CmsVEC3 {
     /// Initializes a new vector
@@ -36,5 +41,74 @@ impl CmsVEC3 {
 
     pub(super) fn as_array(&self) -> [f64; 3] {
         [self.x, self.y, self.z]
+    }
+}
+
+use std::ops::{Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Neg};
+impl Add for CmsVEC3 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self::new(self.x + other.x, self.y + other.y, self.z + other.z)
+    }
+}
+impl AddAssign for CmsVEC3 {
+    fn add_assign(&mut self, other: Self) {
+        self.x += other.x;
+        self.y += other.y;
+        self.z += other.z;
+    }
+}
+impl Sub for CmsVEC3 {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self::new(self.x - other.x, self.y - other.y, self.z - other.z)
+    }
+}
+impl SubAssign for CmsVEC3 {
+    fn sub_assign(&mut self, other: Self) {
+        self.x -= other.x;
+        self.y -= other.y;
+        self.z -= other.z;
+    }
+}
+impl Mul for CmsVEC3 {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        self.cross(&other)
+    }
+}
+impl MulAssign for CmsVEC3 {
+    fn mul_assign(&mut self, other: Self) {
+        let result = Self {
+            x: self.y * other.z - other.y * self.z,
+            y: self.z * other.x - other.z * self.x,
+            z: self.x * other.y - other.x * self.y,
+        };
+        self.x = result.x;
+        self.y = result.y;
+        self.z = result.z;
+    }
+}
+impl Neg for CmsVEC3 {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self::new(-self.x, -self.y, -self.z)
+    }
+}
+
+use std::fmt::{Formatter, Result, Display};
+impl Display for CmsVEC3 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
+}
+
+use std::hash::{Hash, Hasher};
+impl Hash for CmsVEC3 {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        f64::to_be_bytes(self.x).hash(state);
+        f64::to_be_bytes(self.y).hash(state);
+        f64::to_be_bytes(self.z).hash(state);
+        f64::to_be_bytes(420.69).hash(state);
     }
 }
