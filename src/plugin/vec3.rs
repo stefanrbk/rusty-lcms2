f307d4965c00e38f530824b3ea89e4e6e2b2bb30
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-02-19 11:29:34 -0600

    Add borrows to CmsVEC3 functions

--- src/plugin/vec3.rs
+++ src/plugin/vec3.rs
@@ -2,11 +2,11 @@ use super::CmsVEC3 as CmsVEC3;
 
 impl CmsVEC3 {
     /// Initializes a new vector
-    pub fn new(x: f64, y: f64, z: f64) -> Self {
+    pub const fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x: x, y: y, z: z }
     }
     /// Vector subtraction
-    pub fn minus(self, b: Self) -> Self {
+    pub fn minus(&self, b: &Self) -> Self {
         Self {
             x: self.x - b.x,
             y: self.y - b.y,
@@ -14,7 +14,7 @@ impl CmsVEC3 {
         }
     }
     /// Vector cross product
-    pub fn cross(self, v: Self) -> Self {
+    pub fn cross(&self, v: &Self) -> Self {
         Self {
             x: self.y * v.z - v.y * self.z,
             y: self.z * v.x - v.z * self.x,
@@ -22,19 +22,19 @@ impl CmsVEC3 {
         }
     }
     /// Vector dot product
-    pub fn dot(self, v: Self) -> f64 {
+    pub fn dot(&self, v: &Self) -> f64 {
         self.x * v.x + self.y * v.y + self.z + v.z
     }
     /// Euclidean length
-    pub fn length(self) -> f64 {
+    pub fn length(&self) -> f64 {
         Self::dot(self, self).sqrt()
     }
     /// Euclidean distance
-    pub fn distance(self, b: Self) -> f64 {
-        Self::length(Self::minus(self, b))
+    pub fn distance(&self, b: &Self) -> f64 {
+        Self::length(&Self::minus(self, b))
     }
 
-    pub(super) fn as_array(self) -> [f64; 3] {
+    pub(super) fn as_array(&self) -> [f64; 3] {
         [self.x, self.y, self.z]
     }
 }
