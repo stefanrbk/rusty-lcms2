8354f118a21012ba7242e382d36d3c5aee94155f
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-03-05 22:13:38 -0600

    Add traits to Signatures

--- src/signature.rs
+++ src/signature.rs
@@ -1,8 +1,30 @@
+use std::fmt;
+
+#[derive(Copy, Clone, Eq, PartialEq)]
 pub struct Signature(u32);
 
 impl Signature {
     pub const fn new(value: &[u8; 4]) -> Signature {
         Self(u32::from_be_bytes(*value))
+    }
+}
+impl fmt::Debug for Signature {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let bytes = u32::to_be_bytes(self.0);
+        let value: String = match std::str::from_utf8(&bytes) {
+            Ok(i) => i.to_string(),
+            Err(_) => {
+                let mut line = String::with_capacity(16);
+                for i in 0..4 {
+                    line += format!(" {:2x} ", bytes[i]);
+                }
+                line
+            },
+        };
+
+        f.debug_struct("Signature")
+         .field("value", &value)
+         .finish()
     }
 }
 impl Default for Signature {
@@ -56,7 +78,7 @@ impl From<&[u8]> for Signature {
         let len = value.len();
         let mut result: [u8; 4] = [0x20; 4];
         match len {
-            i if i <= 0 => result = [0,0,0,0],
+            i if i <= 0 => result = [0, 0, 0, 0],
             i if i == 1 => result[..1].copy_from_slice(&value[..1]),
             i if i == 2 => result[..2].copy_from_slice(&value[..2]),
             i if i == 3 => result[..3].copy_from_slice(&value[..3]),
