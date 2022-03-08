56357273b0a97d88f7817befed8373d62956bc8f
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-03-08 00:55:14 -0600

    Move error messages into internal mod

--- src/internal/mod.rs
+++ src/internal/mod.rs
@@ -1,3 +1,16 @@
+use std::io::*;
+
 mod types;
+
+pub const eof_error: Error = Error::new(
+        ErrorKind::UnexpectedEof,
+        "Can't read from buffer. Unexpected EOF.",
+);
+
+pub const invalid_data_error: Error =
+    Error::new(
+        ErrorKind::InvalidData,
+        "Encountered invalid data.",
+);
 
 pub const MATRIX_DET_TOLERANCE: f64 = 0.0001;
