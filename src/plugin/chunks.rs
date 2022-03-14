7eef01c9d6c95e00e360caa3bc3660ea2ccb0a65
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-03-14 00:40:01 -0500

    Create new functions for chunks

--- src/plugin/chunks.rs
+++ src/plugin/chunks.rs
@@ -1,9 +1,16 @@
-use crate::plugin::error::LogErrorHandlerFunction;
+use crate::plugin::error::*;
 use crate::plugin::*;
 use crate::Signature;
 
 pub struct LogErrorChunk {
     pub handler: LogErrorHandlerFunction,
+}
+impl LogErrorChunk {
+    pub const fn new(func: Option<LogErrorHandlerFunction>) -> Self {
+        Self {
+            handler: func.unwrap_or(default_log_error_handler_function),
+        }
+    }
 }
 
 pub struct TagPluginChunk {
@@ -11,6 +18,11 @@ pub struct TagPluginChunk {
 }
 
 impl TagPluginChunk {
+    pub const fn new() -> Self {
+        Self {
+            tag: Vec::new(),
+        }
+    }
     pub fn get_tag_descriptor(&self, signature: Signature) -> Option<&TagDescriptor> {
         for i in self.tag.iter() {
             if i.sig == signature {
